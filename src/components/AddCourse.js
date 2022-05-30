ce0e93fd0e4c821b68d4ecc8e039bf29f4ab8863
marvinorias <oriasmarvinm@gmail.com>
2022-05-30 17:21:14 +0800

    Modal add course working

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -1,29 +1,30 @@
-import { Button, Modal, Form } from 'react-bootstrap';
 import { useState } from 'react';
+import { Button, Modal, Form } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 
-export default function AddCourse({fetchData}){
+export default function AddCourse({fetchData}) {
 
-	//Add state for forms of adding a course
+	//Add state for the forms of adding a course
 	const [ name, setName ] = useState('');
-	const [description, setDescription] = useState('');
-	const [price, setPrice] = useState(0);
+	const [ description, setDescription ] = useState('');
+	const [ price, setPrice ] = useState(0);
 
-	//states for opening and closing modals
-	const [showAdd, setShowAdd] = useState(false);
+	//States for opening and closing the modals
+	const [ showAdd, setShowAdd ] = useState(false);
 
-	//functions to handle opening and closing of modal
+	//Functions to handle opening and closing of our Modal
 	const openAdd = () => setShowAdd(true);
-	const closeAdd = () => setShowAdd(false);
+	const closeAdd = () => setShowAdd(false); 
 
+	//function for adding a course
 	const addCourse = (e) => {
 		e.preventDefault();
 
 		fetch('http://localhost:4000/products/create', {
-			method: "POST",
+			method: 'POST',
 			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${ localStorage.getItem('accessToken') }`
 			},
 			body: JSON.stringify({
 				name: name,
@@ -37,80 +38,96 @@ export default function AddCourse({fetchData}){
 
 			if(data){
 				Swal.fire({
-					title: "Success",
-					icon: "success",
-					text: "Course successfully added"
-				})
-				//close modal
-				closeAdd();
-				//window.location.reload()
-				fetchData();
-			}
-			else{
-				Swal.fire({
-					title: "Error",
-					icon: "error",
-					text: "Please try again"
+					title: 'Success',
+					icon: 'success',
+					text: 'Course successfully added'
 				})
 
-				fetchData();
+				//Close our modal
+				closeAdd()
+
+				fetchData()
+			}else {
+				Swal.fire({
+					title: 'error',
+					icon: 'error',
+					text: 'Please try again'
+				})
+
+				fetchData()
 			}
 
 			//reset all states input
-			setName('');
-			setDescription('');
-			setPrice(0);
-
+			setName('')
+			setDescription('')
+			setPrice(0)
 		})
+
+
 	}
 
 	return(
-			<>
-				<Button variant="primary">Add New Course</Button>
+		<>
+			<Button variant="primary" onClick={openAdd}>Add New Course</Button>
 
-				{/*//Add modal*/}
+			{/* Add Modal */}
 
-				<Modal show={showAdd} onHide={closeAdd}>
-					<Form onSubmit={e => addCourse(e)}>
-						<Modal.Header closeButton>
-							<Modal.Title>Add Course</Modal.Title>
-						</Modal.Header>
+			<Modal show={showAdd} onHide={closeAdd}>
+				<Form onSubmit={e => addCourse(e)}>
+					<Modal.Header closeButton>
+						<Modal.Title>Add Course</Modal.Title>
+					</Modal.Header>
 
-						<Modal.Body>
-							<Form.Group>
-								<Form.Label>Name</Form.Label>
-								<Form.Control type="text"
-								required
-								value={name}
-								onChange={e => setName(e.target.value)}
-								/>
-							</Form.Group>
+					<Modal.Body>
+						<Form.Group>
+							<Form.Label>Name</Form.Label>
+							<Form.Control 
+							      type="text"
+							      required
+							      value={name}
+							      onChange={e => setName(e.target.value)}
+							 />
+						</Form.Group>
 
-							<Form.Group>
-								<Form.Label>description</Form.Label>
-								<Form.Control type="text"
-								required
-								value={description}
-								onChange={e => setName(e.target.value)}
-								/>
-							</Form.Group>
+						<Form.Group>
+							<Form.Label>Description</Form.Label>
+							<Form.Control 
+							      type="text"
+							      required
+							      value={description}
+							      onChange={e => setDescription(e.target.value)}
+							 />
+						</Form.Group>
 
-							<Form.Group>
-								<Form.Label>Price</Form.Label>
-								<Form.Control type="text"
-								required
-								value={price}
-								onChange={e => setName(e.target.value)}
-								/>
-							</Form.Group>
-						</Modal.Body>
+						<Form.Group>
+							<Form.Label>Price</Form.Label>
+							<Form.Control 
+							      type="number"
+							      required
+							      value={price}
+							      onChange={e => setPrice(e.target.value)}
+							 />
+						</Form.Group>
+					</Modal.Body>
 
-						<Modal.Footer>
-							<Button variatn="secondary" onClick={closeAdd}>Close</Button>
-							<Button variatn="success" type="submit" >Submit</Button>
-						</Modal.Footer>
-					</Form>
-				</Modal>
-			</>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={closeAdd}>Close</Button>
+						<Button variant="success" type="submit">Submit</Button>
+					</Modal.Footer>
+
+				</Form>
+			</Modal>
+		</>
+
+
+
+
+
+
+
+
+
+
+
 		)
 }
